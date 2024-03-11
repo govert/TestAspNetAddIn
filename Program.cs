7309f873312b529e07b3ba910fa34b785205ba48
Govert van Drimmelen <govert@dnakode.com>
2024-03-11 14:09:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+
+using ExcelDna.Integration;
+
+namespace TestAspNetAddIn
+{
+    public class AddIn : IExcelAddIn
+    {
+        WebApplication app;
+
+        public void AutoOpen()
+        {
+            var builder = WebApplication.CreateBuilder();
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+
+            app.RunAsync("http://localhost:5144");
+        }
+
+        public void AutoClose()
+        {
+            
+        }
+
+    }
+}
